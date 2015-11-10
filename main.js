302b447873b7d5326e068f83dc020ebd2bee0926
Carlos Navas <carlosnavas1991@gmail.com>
2015-11-10 20:03:22 +0100

    Version 0.2

--- main.js
+++ main.js
@@ -34,4 +34,30 @@ $( document ).ready(function() {
 
   });
 
+  $('.ui-checkbox').on('click', function(){
+
+    var object = $(this);
+    if(!object.hasClass('inactive')){
+      if(object.hasClass('active')){
+        object.removeClass('active');
+      }else{
+        object.addClass('active');
+      }
+    }
+
+  });
+
+  $('.ui-radio-button').on('click', function(){
+
+    var object = $(this);
+    if(!object.hasClass('inactive')){
+      if(object.hasClass('active')){
+        object.removeClass('active');
+      }else{
+        object.addClass('active');
+      }
+    }
+
+  });
+
 });
