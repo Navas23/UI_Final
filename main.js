87f09bf74c3e57c34c8933d183d32710e7c9037b
Carlos Navas <carlosnavas1991@gmail.com>
2015-11-10 19:14:11 +0100

    Version 0.1

---
+++ main.js
@@ -0,0 +1,37 @@
+$( document ).ready(function() {
+
+  var uiMenu = $('.ui-context-menu.example');
+  var zIndex = 1;
+
+  $('.ui-view').on('contextmenu', function(e){
+
+    zIndex++;
+    console.log(e.pageX+ ' , ' + e.pageY);
+    e.preventDefault();
+    var width   = e.pageX;
+    var heigth  = e.pageY;
+    console.log(uiMenu);
+    uiMenu.clone().css({
+      "zIndex" : zIndex,
+      "position": "absolute",
+      "left": e.pageX,
+      "top": e.pageY
+    }).insertAfter($('.ui-view'));
+
+  });
+
+  $('.ui-context-menu').on('click', function(e){
+    console.log( $(this) );
+    $( this ).style.animation
+  });
+
+
+  $(document).keypress( function(e){
+
+    if(e.which === 100){
+        $('.ui-view').toggleClass('dark');
+    }
+
+  });
+
+});
